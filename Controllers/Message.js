be98a3427c81ca21a0d1724b4c8f921d27e7c35d
Nwachukwu_Uzor <uzor1997@gmail.com>
2022-03-13 21:48:13 +0100

    adding delete route, get single message route and readme file

--- Controllers/Message.js
+++ Controllers/Message.js
@@ -6,16 +6,28 @@ const createMessage = async(req, res) => {
         const newMessage = await Message.create({name, age, message});
         res.status(201).json({message: 'message created successfully', data: newMessage})
     } catch(error) {
-        res.status(400).json({message: error.message})
+        res.status(400).json({message: "Invalid message", error: error.message})
     }
 }
 
 const getAllMessages = async(req, res) => {
     const messages = await Message.find();
     try {
-        res.status(200).json(messages)
+        res.status(200).json({data: messages})
     } catch {
         res.status(404).json({message: 'Not found'})
+    }
+}
+
+const getSingleMessage = async(req, res) => {
+    const {id} = req.params;
+
+    const message = await Message.findById(id);
+
+    try {
+        res.status(200).json({data: message})
+    } catch (error) {
+        res.status(404).json({message: "Error invalid message id"})
     }
 }
 
@@ -27,12 +39,25 @@ const updateMessage = async(req, res) => {
         await Message.findByIdAndUpdate(id, {message})
         res.status(200).json({message: "update successful"})
     } catch (error) {
-        res.status(401).json({message: error.message})
+        res.status(401).json({message: "Invalid Id"})
+    }
+}
+
+const deleteMessage = async(req, res) => {
+    const {id} = req.params;
+
+    try {
+        await Message.findByIdAndDelete(id);
+        res.status(200).json({message: "message deleted successfully"})
+    } catch (error) {
+        res.status(401).json({message: "Invalid Id"})
     }
 }
 
 module.exports = {
     createMessage,
     getAllMessages,
-    updateMessage
+    getSingleMessage,
+    updateMessage,
+    deleteMessage
 }
--- Routes/Message.js
+++ Routes/Message.js
@@ -4,8 +4,9 @@ const MessageControllers = require('../Controllers/Message.js');
 const MessageRouter = express.Router();
 
 MessageRouter.get('/', MessageControllers.getAllMessages);
+MessageRouter.get('/:id', MessageControllers.getSingleMessage);
 MessageRouter.post('/', MessageControllers.createMessage);
 MessageRouter.patch('/:id', MessageControllers.updateMessage);
-
+MessageRouter.delete('/:id', MessageControllers.deleteMessage);
 
 module.exports = MessageRouter;
